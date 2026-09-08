a5c31509084e6fe3d4bd485320f6a26bbdf54c33
SebastianSPM <sebasperemald04@gmail.com>
2026-09-08 16:10:53 -0500

    Creando el repositorio CanchaServicioRepository

--- src/main/java/com/generation/grupo10/repository/CanchaServicioRepository.java
+++ src/main/java/com/generation/grupo10/repository/CanchaServicioRepository.java
@@ -1,4 +1,11 @@
 package com.generation.grupo10.repository;
 
-public class CanchaServicioRepository {
+import com.generation.grupo10.model.CanchaServicio;
+import com.generation.grupo10.model.CanchaServicioId;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CanchaServicioRepository
+        extends JpaRepository<CanchaServicio, CanchaServicioId> {
 }
