3836690f23c21773e4363772686ecae23a3f8e01
DylanPuentes <dfelipe.puentes@gmail.com>
2026-09-08 16:30:33 -0500

    Correcion para controller de service

--- src/main/java/com/generation/grupo10/controller/ServicioController.java
+++ src/main/java/com/generation/grupo10/controller/ServicioController.java
@@ -1,8 +1,7 @@
-package com.cancheros.controller;
+package com.generation.grupo10.controller;
 
-import com.cancheros.dto.ServicioDTO;
-import com.cancheros.model.Servicio;
-import com.cancheros.service.ServicioService;
+import com.generation.grupo10.model.Servicio;
+import com.generation.grupo10.service.ServicioService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,26 +19,18 @@ public class ServicioController {
         this.servicioService = servicioService;
     }
 
-    // GET: Obtener lista de todos los servicios para los checkboxes del formulario
     @GetMapping
     public ResponseEntity<List<Servicio>> listar() {
         return ResponseEntity.ok(servicioService.listarTodos());
     }
 
-    // POST: Crear un nuevo servicio
+
     @PostMapping
-    public ResponseEntity<Servicio> crear(@RequestBody ServicioDTO dto) {
-        return new ResponseEntity<>(servicioService.crearServicio(dto), HttpStatus.CREATED);
+    public ResponseEntity<Servicio> crear(@RequestBody Servicio servicio) {
+        return new ResponseEntity<>(servicioService.crearServicio(servicio), HttpStatus.CREATED);
     }
 
-    // POST: Asociar un servicio existente a una cancha específica
-    @PostMapping("/{servicioId}/asociar/{canchaId}")
-    public ResponseEntity<Void> asociarACancha(@PathVariable Long servicioId, @PathVariable Long canchaId) {
-        servicioService.asignarServicioACancha(canchaId, servicioId);
-        return ResponseEntity.ok().build();
-    }
 
-    // DELETE: Eliminar un servicio
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> eliminar(@PathVariable Long id) {
         servicioService.eliminarServicio(id);
