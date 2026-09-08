0e6f469438da9e998e5daff06dccd13e25285323
SebastianSPM <sebasperemald04@gmail.com>
2026-09-08 10:12:11 -0500

    Estructura inicial

---
+++ src/test/java/com/generation/grupo10/Grupo10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.grupo10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
