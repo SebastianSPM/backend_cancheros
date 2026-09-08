08e7ecd370485ccaca016f62af3c47b891b62831
SebastianSPM <sebasperemald04@gmail.com>
2026-09-08 17:45:32 -0500

    Creando UsuarioService

---
+++ src/main/java/com/generation/grupo10/service/UsuarioService.java
@@ -0,0 +1,34 @@
+package com.generation.grupo10.service;
+
+import com.generation.grupo10.model.Usuario;
+import com.generation.grupo10.repository.UsuarioRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UsuarioService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    public UsuarioService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    public List<Usuario> listarUsuarios() {
+        return usuarioRepository.findAll();
+    }
+
+    public Optional<Usuario> buscarPorId(Long id) {
+        return usuarioRepository.findById(id);
+    }
+
+    public Usuario guardarUsuario(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
+
+    public void eliminarUsuario(Long id) {
+        usuarioRepository.deleteById(id);
+    }
+}
