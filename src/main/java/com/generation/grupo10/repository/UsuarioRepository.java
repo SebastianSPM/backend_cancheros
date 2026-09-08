cdbf5e8dd2aad2f734fe1cd74daa62ee84f1a7de
SebastianSPM <sebasperemald04@gmail.com>
2026-09-08 17:42:22 -0500

    Creando UsuarioRepository

---
+++ src/main/java/com/generation/grupo10/repository/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package com.generation.grupo10.repository;
+
+import com.generation.grupo10.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+}
